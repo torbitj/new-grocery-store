c8db637c0db728be3309f9a86cc8b011359f641d
JT <jstorbit@gmail.com>
2025-11-12 10:08:16 -0700

    Add price for each item

--- index.js
+++ index.js
@@ -2,105 +2,105 @@ const groceryItems = [
   {
     id: 1,
     name: `apple`,
-    price: null,
+    price: 1.49,
     category: null,
     quantity: null
   },
   {
     id: 2,
     name: `banana`,
-    price: null,
+    price: 0.30,
     category: null,
     quantity: null
   },
   {
     id: 3,
     name: `avocado`,
-    price: null,
+    price: 1.50,
     category: null,
     quantity: null
   },
   {
     id: 4,
     name: `squash`,
-    price: null,
+    price: 2.49,
     category: null,
     quantity: null
   },
   {
     id: 5,
     name: `zucchini`,
-    price: null,
+    price: 1.99,
     category: null,
     quantity: null
   },
   {
     id: 6,
     name: `sweet potatoes`,
-    price: null,
+    price: 2.99,
     category: null,
     quantity: null
   },
   {
     id: 7,
     name: `potato chips`,
-    price: null,
+    price: 2.49,
     category: null,
     quantity: null
   },
   {
     id: 8,
     name: `granola bar`,
-    price: null,
+    price: 2.99,
     category: null,
     quantity: null
   },
   {
     id: 9,
     name: `fruit snacks`,
-    price: null,
+    price: 4.99,
     category: null,
     quantity: null
   },
   {
     id: 10,
     name: `cheese`,
-    price: null,
+    price: 5.49,
     category: null,
     quantity: null
   },
   {
     id: 11,
     name: `butter`,
-    price: null,
+    price: 3.99,
     category: null,
     quantity: null
   },
   {
     id: 12,
     name: `milk`,
-    price: null,
+    price: 2.99,
     category: null,
     quantity: null
   },
   {
     id: 13,
     name: `chicken`,
-    price: null,
+    price: 5.99,
     category: null,
     quantity: null
   },
   {
     id: 14,
     name: `steak`,
-    price: null,
+    price: 10.99,
     category: null,
     quantity: null
   },
   {
     id: 15,
     name: `ham`,
-    price: null,
+    price: 9.99,
     category: null,
     quantity: null
   },
