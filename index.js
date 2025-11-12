c93030aca95a4001ce66d7dd625d7b2b42ece13e
JT <jstorbit@gmail.com>
2025-11-12 10:14:51 -0700

    Add category for each item

--- index.js
+++ index.js
@@ -3,105 +3,105 @@ const groceryItems = [
     id: 1,
     name: `apple`,
     price: 1.49,
-    category: null,
+    category: `fruit`,
     quantity: null
   },
   {
     id: 2,
     name: `banana`,
     price: 0.30,
-    category: null,
+    category: `fruit`,
     quantity: null
   },
   {
     id: 3,
     name: `avocado`,
     price: 1.50,
-    category: null,
+    category: `fruit`,
     quantity: null
   },
   {
     id: 4,
     name: `squash`,
     price: 2.49,
-    category: null,
+    category: `vegetable`,
     quantity: null
   },
   {
     id: 5,
     name: `zucchini`,
     price: 1.99,
-    category: null,
+    category: `vegetable`,
     quantity: null
   },
   {
     id: 6,
     name: `sweet potatoes`,
     price: 2.99,
-    category: null,
+    category: `vegetable`,
     quantity: null
   },
   {
     id: 7,
     name: `potato chips`,
     price: 2.49,
-    category: null,
+    category: `snack`,
     quantity: null
   },
   {
     id: 8,
     name: `granola bar`,
     price: 2.99,
-    category: null,
+    category: `snack`,
     quantity: null
   },
   {
     id: 9,
     name: `fruit snacks`,
     price: 4.99,
-    category: null,
+    category: `snack`,
     quantity: null
   },
   {
     id: 10,
     name: `cheese`,
     price: 5.49,
-    category: null,
+    category: `dairy`,
     quantity: null
   },
   {
     id: 11,
     name: `butter`,
     price: 3.99,
-    category: null,
+    category: `dairy`,
     quantity: null
   },
   {
     id: 12,
     name: `milk`,
     price: 2.99,
-    category: null,
+    category: `dairy`,
     quantity: null
   },
   {
     id: 13,
     name: `chicken`,
     price: 5.99,
-    category: null,
+    category: `meat`,
     quantity: null
   },
   {
     id: 14,
     name: `steak`,
     price: 10.99,
-    category: null,
+    category: `meat`,
     quantity: null
   },
   {
     id: 15,
     name: `ham`,
     price: 9.99,
-    category: null,
+    category: `meat`,
     quantity: null
   },
 ];
